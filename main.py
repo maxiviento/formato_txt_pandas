267a98beb85c8292446e3d2a6bc10436434c6945
edgarlunaa <edgaribarluna@gmail.com>
2021-02-23 10:36:10 -0300

    arreglo dinamico

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from openpyxl import load_workbook
 import pandas as pd
 
-codigo_empresa = '5541'
+codigo_empresa = '5533'
 
 def dar_formato(txt, total, tipo):
     if len(txt) + 1 <= total:
@@ -30,7 +30,7 @@ for row in ws_formato:
     except:
         pass
 
-col_list = ['N_DEPARTAMENTO_GU', 'localidad', 'COD_SUCURSAL', 'NOMBRE_SUCURSAL', 'NOMBRES', 'APELLIDOS', 'CUIT', 'FECHA_DESDE', 'FECHA_HASTA', 'NRO_DOCUMENTO']
+col_list = ['MONTO', 'N_DEPARTAMENTO_GU', 'localidad', 'NUMERO DE SUCURSAL', 'NOMBRE SUCURSAL', 'NOMBRES', 'APELLIDOS', 'CUIT', 'FECHA_DESDE', 'FECHA_HASTA', 'NRO_DOCUMENTO']
 df_cuotas = pd.read_excel(codigo_empresa+'.xlsx', usecols=col_list)
 
 print(list(df_cuotas.columns))
@@ -38,7 +38,7 @@ print(list(df_cuotas.columns))
 try:
     df_cuotas['FECHA_HASTA'] = '0' + df_cuotas['FECHA_HASTA'].astype(str)
     df_cuotas['FECHA_DESDE'] = '0' + df_cuotas['FECHA_DESDE'].astype(str)
-    df_cuotas['COD SUCURSAL'] = df_cuotas['COD SUCURSAL'].astype(str).str.slice(0, -2, 1)
+    df_cuotas['NUMERO DE SUCURSAL'] = df_cuotas['NUMERO DE SUCURSAL'].astype(str).str.slice(0, -2, 1)
     df_cuotas['CUIT'] = df_cuotas['CUIT'].astype(str).astype(str).str.slice(0, -2, 1)
 except:
     pass
@@ -48,9 +48,12 @@ print(df_cuotas)
 df_cuotas.to_excel(codigo_empresa+" formateado.xlsx", index=False)
 
 print(df_cuotas)
-monto_gral = '004128000000'
-cantidad_registros = '00002064'
-txt = "00012021"+cantidad_registros+monto_gral+"000000000000000000000102202105022021020"+ codigo_empresa + dar_formato("", 953, 'A')
+
+calc_monto_gral = df_cuotas['MONTO'].sum()
+monto_gral = dar_formato(str(calc_monto_gral), 10, 'N')
+monto_gral = monto_gral + '00'
+cantidad_registros = dar_formato(str(df_cuotas.shape[0]), 8, 'N')
+txt = "00022021"+cantidad_registros+monto_gral+"000000000000000000000103202105032021020"+ codigo_empresa + dar_formato("", 953, 'A')
 for index, row in df_cuotas.iterrows():
     txt += "\n"
     txt += "00"
@@ -74,7 +77,7 @@ for index, row in df_cuotas.iterrows():
     #APELLIDO Y NOMBRE DE APODERADO
     txt += dar_formato("", 27, 'A')
     #TIPO DOC. APODERADO NRO DOC APODERADO PCIA EMISIÓN: CBA=04	CÓDIGO CONCEPTO 1: FIJO 001	SUBCÓDIGO DEL CONCEPTO 1: FIJO 000	IMPORTE DEL CONCEPTO 1
-    txt += "000000000040010000002000000"
+    txt += "00000000004001000000" + str(row['MONTO']) + '00'
     #columna desde la 23 a la 97
     sum = 0
     for i in range(23,98):
@@ -86,7 +89,11 @@ for index, row in df_cuotas.iterrows():
     #NRO CUENTA VALOR FIJO 0	FECHA DESDE PROX PAGO: DDMMAAA	FECHA HASTA PROX PAGO: DDMMAAAA
     txt += dar_formato("", 36, 'N')
     #LEYENDA 1	LEYENDA 2	LEYENDA 3	LEYENDA 4
-    ministerio = 'MINISTERIO DE DESARROLLO SOCIAL       '
+    if codigo_empresa == '5533':
+        ministerio = 'MINISTERIO DE PROMOCION DEL EMPLEO Y E'
+    elif codigo_empresa == '5541':
+        ministerio = 'MINISTERIO DE DESARROLLO SOCIAL       '
+
     programa = 'PROGRAMA VIDA DIGNA                   '
     txt += "GOBIERNO DE LA PROVINCIA DE CORDOBA   "+ministerio+programa+"APODERADO DE                          "
     #LEYENDA 5
@@ -94,11 +101,9 @@ for index, row in df_cuotas.iterrows():
     #LEYENDA 6	LEYENDA 7	LEYENDA 8
     txt += dar_formato("", 114, 'A')
     #CÓDIGO PAGO-IMPAGO: LA EMPRESA DEBE PONER SIEMPRE 1= IMPAGO. ELBANCO EN LA RENDICIÓN DEVUELVE 0=PAGO O 1=IMPAGO	FECHA PAGO:COMPLETA EL BANCO LA EMPRESA DEBE PONER 0	PAGO CON TARJETA VALOR FIJO = 0	MOTIVO IMPAGO VALOR FIJO PARA LA EMPRESA=0- LUEGO COMPLETA EL BANCO - VER ANEXOS	NÚMERO DE COMPROBANTE: COMPLETA EL BANCO	ÚLTIMO MOV CUENTA: COMPLETA EL BANCO	RETENCIÓN DE TARJETA: 0=PAGO NORMAL; 1=TARJ.RETENIDA CAJERO	COMISIÓN: VALOR FIJO= ESPACIO	UR ASIGNADA: VALOR FIJO=000	IMPORTE  MORATORIA AFIP: VALOR FIJO=0000000000	IMP. RETROACTIVO MOR. AFIP: VALOR FIJO=0000000000	IMPORTE NETO A COBRAR 	CÓDIGO DE EMPRESA
-    txt += "100000000000000000000000000 00000000000000000000000000002000000"+ codigo_empresa
+    txt += "100000000000000000000000000 0000000000000000000000000000" + str(row['MONTO']) + '00' + codigo_empresa
     #USO FUTURO: VALOR FIJO= ESPACIOS
     txt += dar_formato("", 38, 'A')
-
-    
 
 f = open(codigo_empresa+".txt", "w")
 f.write(txt)
